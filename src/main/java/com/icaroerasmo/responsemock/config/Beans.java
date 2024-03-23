0070211fbfce04234eec94551a27d6dc383b11b8
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2024-03-22 22:57:38 -0300

    adds config to disable cors

--- src/main/java/com/icaroerasmo/responsemock/config/Beans.java
+++ src/main/java/com/icaroerasmo/responsemock/config/Beans.java
@@ -9,6 +9,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
 import java.util.UUID;
@@ -23,5 +25,16 @@ public class Beans {
         RedisTemplate<UUID, Endpoint> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         return template;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
